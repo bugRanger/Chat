40a400605765679e45ee4de31e8c9d46f6a71c6d
bugRanger <departmentofjusticeevil@gmail.com>
2021-03-05 12:13:23 +0700

    Fix text api messages.

--- Chat.Tests/TextApiTests.cs
+++ Chat.Tests/TextApiTests.cs
@@ -3,8 +3,6 @@
     using System;
     using NUnit.Framework;
 
-    using Chat.Api;
-
     [TestFixture]
     public class TextApiTests
     {
@@ -28,7 +26,7 @@
         #region Methods
 
         [Test]
-        public void PrivateMessageNotLogginTest()
+        public void Send_IsNotLoggined_Failure()
         {
             // Arrange
             _coreTests.ConnectionTest();
@@ -44,23 +42,7 @@
         }
 
         [Test]
-        public void PrivateMessageNotExistsTargetTest()
-        {
-            // Arrange
-            _coreTests.AuthorizationTest();
-
-            var request = _coreTests.MessageFactory.Pack("{\"Id\":1,\"Type\":\"message\",\"Payload\":{\"Source\":\"User1\",\"Target\":\"User2\",\"Message\":\"Hi!\"}}");
-            _coreTests.ExpectedEvent.Add(new TestEvent(_coreTests.Remotes[0], _coreTests.MessageFactory.Pack("{\"Id\":1,\"Type\":\"result\",\"Payload\":{\"Status\":\"UserNotFound\",\"Reason\":\"Target not found\"}}")));
-
-            // Act
-            _coreTests.NetworkMoq.Raise(s => s.PreparePacket += null, _coreTests.Remotes[0], request, 0, request.Length);
-
-            // Assert
-            CollectionAssert.AreEqual(_coreTests.ExpectedEvent, _coreTests.ActualEvent);
-        }
-
-        [Test]
-        public void PrivateMessageWithInvalidParametersTest()
+        public void Send_MessageIsEmpty_Failure()
         {
             // Arrange
             _coreTests.AuthorizationTest();
@@ -77,7 +59,57 @@
         }
 
         [Test]
-        public void PrivateMessageTest()
+        public void Send_TargetNotExists_Failure()
+        {
+            // Arrange
+            _coreTests.AuthorizationTest();
+
+            var request = _coreTests.MessageFactory.Pack("{\"Id\":1,\"Type\":\"message\",\"Payload\":{\"Source\":\"User1\",\"Target\":\"User2\",\"Message\":\"Hi!\"}}");
+            _coreTests.ExpectedEvent.Add(new TestEvent(_coreTests.Remotes[0], _coreTests.MessageFactory.Pack("{\"Id\":1,\"Type\":\"result\",\"Payload\":{\"Status\":\"UserNotFound\",\"Reason\":\"Target not found\"}}")));
+
+            // Act
+            _coreTests.NetworkMoq.Raise(s => s.PreparePacket += null, _coreTests.Remotes[0], request, 0, request.Length);
+
+            // Assert
+            CollectionAssert.AreEqual(_coreTests.ExpectedEvent, _coreTests.ActualEvent);
+        }
+
+        [Test]
+        public void Send_TargetIsEmpty_Failure()
+        {
+            // Arrange
+            _coreTests.AuthorizationTest();
+
+            var request = _coreTests.MessageFactory.Pack("{\"Id\":1,\"Type\":\"message\",\"Payload\":{\"Source\":\"User1\",\"Target\":\"\",\"Message\":\"Hi!\"}}");
+            _coreTests.ExpectedEvent.Add(new TestEvent(_coreTests.Remotes[0], _coreTests.MessageFactory.Pack("{\"Id\":1,\"Type\":\"result\",\"Payload\":{\"Status\":\"Failure\",\"Reason\":\"Not supported\"}}")));
+
+            // Act
+            _coreTests.NetworkMoq.Raise(s => s.PreparePacket += null, _coreTests.Remotes[0], request, 0, request.Length);
+
+            // Assert
+            CollectionAssert.AreEqual(_coreTests.ExpectedEvent, _coreTests.ActualEvent);
+        }
+
+        [Test]
+        public void Send_SourceIsEmpty_Success()
+        {
+            // Arrange
+            _coreTests.AuthorizationTest();
+            _coreTests.AuthorizationTest();
+
+            var request = _coreTests.MessageFactory.Pack("{\"Id\":1,\"Type\":\"message\",\"Payload\":{\"Source\":\"\",\"Target\":\"User2\",\"Message\":\"Hi!\"}}");
+            _coreTests.ExpectedEvent.Add(new TestEvent(_coreTests.Remotes[0], _coreTests.MessageFactory.Pack("{\"Id\":1,\"Type\":\"result\",\"Payload\":{\"Status\":\"Success\",\"Reason\":\"\"}}")));
+            _coreTests.ExpectedEvent.Add(new TestEvent(_coreTests.Remotes[1], _coreTests.MessageFactory.Pack("{\"Id\":0,\"Type\":\"message\",\"Payload\":{\"Source\":\"User1\",\"Target\":\"User2\",\"Message\":\"Hi!\"}}")));
+
+            // Act
+            _coreTests.NetworkMoq.Raise(s => s.PreparePacket += null, _coreTests.Remotes[0], request, 0, request.Length);
+
+            // Assert
+            CollectionAssert.AreEqual(_coreTests.ExpectedEvent, _coreTests.ActualEvent);
+        }
+
+        [Test]
+        public void Send_Correct_Success()
         {
             // Arrange
             _coreTests.AuthorizationTest();
@@ -85,8 +117,7 @@
 
             var request = _coreTests.MessageFactory.Pack("{\"Id\":1,\"Type\":\"message\",\"Payload\":{\"Source\":\"User1\",\"Target\":\"User2\",\"Message\":\"Hi!\"}}");
             _coreTests.ExpectedEvent.Add(new TestEvent(_coreTests.Remotes[0], _coreTests.MessageFactory.Pack("{\"Id\":1,\"Type\":\"result\",\"Payload\":{\"Status\":\"Success\",\"Reason\":\"\"}}")));
-            _coreTests.ExpectedEvent.Add(new TestEvent(_coreTests.Remotes[1],
-                _coreTests.MessageFactory.Pack("{\"Id\":0,\"Type\":\"message\",\"Payload\":{\"Source\":\"User1\",\"Target\":\"User2\",\"Message\":\"Hi!\"}}")));
+            _coreTests.ExpectedEvent.Add(new TestEvent(_coreTests.Remotes[1], _coreTests.MessageFactory.Pack("{\"Id\":0,\"Type\":\"message\",\"Payload\":{\"Source\":\"User1\",\"Target\":\"User2\",\"Message\":\"Hi!\"}}")));
 
             // Act
             _coreTests.NetworkMoq.Raise(s => s.PreparePacket += null, _coreTests.Remotes[0], request, 0, request.Length);
