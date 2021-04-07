4d8ae3e4c04a8d2ad3ab695a8a182b467d79a139
Anikaevni <Anikaevni@ANIKAEVNI>
2021-04-07 14:41:19 +0700

    Add new impl jitter.

--- Chat.Net/Jitter/JitterQueue.cs
+++ Chat.Net/Jitter/JitterQueue.cs
@@ -10,12 +10,13 @@
         private readonly object _locker;
 
         private readonly uint _limit;
+        private readonly uint _dropped;
         private readonly T[] _packets;
 
-        private uint _indexPull;
-        private uint _indexPush;
-        private uint _reordered;
-        private uint _losses;
+        private uint? _indexPull;
+        private int? _indexPush;
+        private int _losses;
+        private bool _marker;
 
         #endregion Fields
 
@@ -27,11 +28,7 @@
 
             _packets = new T[limit];
             _limit = limit;
-
-            _indexPull = int.MaxValue;
-            _indexPush = 0;
-            _reordered = 0;
-            _losses = 0;
+            _dropped = limit * 2;
         }
 
         #endregion Constructors
@@ -42,23 +39,41 @@
         {
             lock (_locker)
             {
-                var index = packet.SequenceId % _limit;
-                var current = _packets[index];
+                int delta = 0;
 
-                if (current != null)
-                    return;
+                if (_indexPush.HasValue)
+                {
+                    delta = (int)(packet.SequenceId - _indexPush);
 
-                if (packet.Mark)
+                    if (Math.Abs(delta) >= _limit)
+                    {
+                        if (delta < 0 && -delta < _dropped)
+                        {
+                            return;
+                        }
+
+                        Clear();
+                        delta = 0;
+                    }
+                }
+
+                if (!_indexPush.HasValue)
+                {
+                    _indexPush = (int)packet.SequenceId;
+                }
+
+                if (packet.Mark || !_marker && delta < 0)
+                {
                     _indexPull = packet.SequenceId;
+                    _marker |= packet.Mark;
+                }
 
-                bool ordered = packet.SequenceId == _indexPush + 1;
-                if (ordered)
-                    _reordered = 0;
-                else
-                    _reordered += 1;
+                _packets[(_indexPush.Value + delta) % _limit] = packet;
 
-                _packets[index] = packet;
-                _indexPush = packet.SequenceId;
+                if (delta > 0)
+                {
+                    _indexPush += delta;
+                }
             }
         }
 
@@ -68,43 +83,34 @@
 
             lock (_locker)
             {
+                if (!_indexPull.HasValue)
+                    return null;
+
                 if (hungry)
-                {
-                    _losses += 1;
-                }
+                    _losses++;
 
-                if (hungry && _losses != 0 && _losses <= _limit)
-                {
+                if (hungry && _losses != 0 && _losses < _limit)
                     return null;
-                }
 
-                if (!hungry && _reordered != 0 && _reordered < _limit)
-                {
+                var index = _indexPull.Value % _limit;
+
+                packet = _packets[index];
+                if (!hungry && packet == null)
                     return null;
-                }
 
-                var index = _indexPull;
-                packet = _packets[index % _limit];
-
-                if (packet == null)
+                try
                 {
-                    if (hungry)
-                    {
-                        return index;
-                    }
+                    _packets[index] = default;
 
-                    return null;
+                    if (!hungry && packet != null)
+                        _losses = 0;
+
+                    return _indexPull;
                 }
-
-                _packets[index % _limit] = default;
-                _indexPull += 1;
-
-                if (!hungry && packet != null)
+                finally
                 {
-                    _losses = 0;
+                    _indexPull++;
                 }
-
-               return index;
             }
         }
 
@@ -112,7 +118,9 @@
         {
             lock (_locker)
             {
-                return _packets[_indexPull % _limit];
+                return _indexPull.HasValue 
+                    ? _packets[_indexPull.Value % _limit] 
+                    : default;
             }
         }
 
@@ -125,10 +133,10 @@
                     _packets[i] = default;
                 }
 
-                _indexPull = int.MaxValue;
-                _indexPush = 0;
-                _reordered = 0;
+                _indexPull = null;
+                _indexPush = null;
                 _losses = 0;
+                _marker = false;
             }
         }
 
